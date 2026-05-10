2703b5b7f8c5b6281662c22ce9015f9d0094ac1a
andrealo2432 <andrealo22432@gmail.com>
2026-05-10 17:14:09 -0500

    change to get_question

--- survey.py
+++ survey.py
@@ -11,10 +11,9 @@ def get_question(question_file):
     question_list = []
     with open(question_file, "r") as fh:
         for question in fh:
-            question = question.strip().split(",")
-            question_text = question[0]
-            letter = question[1]
-            question_list.append((question_text, letter))
+            question = question.strip()
+            if question and not question.startswith("###"):
+                question_list.append((question))
     return question_list
 
 
